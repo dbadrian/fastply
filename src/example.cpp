08d6fa24f74d6804523a5e8bef37ee2bbb984a89
David B. Adrian <dawidh.adrian@googlemail.com>
2018-10-18 17:50:54 +0200

    update example

--- src/example.cpp
+++ src/example.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <array>
 #include <vector>
+#include <chrono>
+
 
 
 #include "fastply.h"
@@ -34,6 +36,7 @@ int main(int argc, char** argv)
 {
     std::cout << "fastply example" << std::endl;
 
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
     fastply::FastPly<VertexLayout, FaceLayout> fp;
     auto path = std::string(argv[1]);
     std::cout << ":: Input: " << path << std::endl;
@@ -42,10 +45,29 @@ int main(int argc, char** argv)
         auto vh = fp.getVertexHeader();
         std::cout << vh.size << std::endl;
 
-        std::vector<int> test(vh.size);
-        std::for_each(vh.start, vh.end, [test](VertexLayout i) mutable { test.push_back(i.x); } );
+        std::vector<float> test;
 
+        for (int j = 0; j < 10; ++j)
+        {
+            auto ptr = vh.start;
+            while(ptr++ != vh.end)
+            {
+                test.push_back(ptr->x*j);
+                test.push_back(ptr->nx*j);
+                test.push_back(ptr->y*j);
+                test.push_back(ptr->ny*j);
+                test.push_back(ptr->z*j);
+                test.push_back(ptr->nz*j);
+            }
+            std::cout << test.size() << std::endl;
+        }
         std::cout << test.size() << std::endl;
+
     }
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+
+    std::cout << duration << std::endl;
     return 0;
 }
