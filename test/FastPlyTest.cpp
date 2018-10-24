065f2a75ebe3e049196d6708637ed4432e04b96d
David B. Adrian <dawidh.adrian@googlemail.com>
2018-10-24 22:30:43 +0200

    [unit_test]: Added testcase for multiple instances (of same file) not leading to unexpected behaviour

--- test/FastPlyTest.cpp
+++ test/FastPlyTest.cpp
@@ -136,10 +136,36 @@ TEST_F(FastPlyEmptyElements, PointerState)
     ASSERT_EQ(fp->get<Face>().begin(), fp->get<Face>().end());
 }
 
+/********************************************************************
+ * Verifies that multiple instances don't conflict, in particular   *
+ * when opened on the same file.                                    *
+ *******************************************************************/
+class FastPlyMultiInstance : public testing::Test {
 
-// TODO: Test multiple instances <different> files
-// TODO: Test multiple instances <different> files
+    using FastPlyC = FastPly<Vertex, Camera, Alltypes, Face>;
 
+    void SetUp() override {
+        fp1 = std::make_unique<FastPlyC>();
+        fp2 = std::make_unique<FastPlyC>();
+    }
+
+public:
+    std::unique_ptr<FastPlyC> fp1;
+    std::unique_ptr<FastPlyC> fp2;
+};
+
+TEST_F(FastPlyMultiInstance, PointerState)
+{
+    auto path = std::string("test_many.ply");
+    ASSERT_EQ(fp1->open(path), true);
+    ASSERT_EQ(fp2->open(path), true);
+
+    auto& el1 = fp1->get<Vertex>();
+    auto& el2 = fp2->get<Vertex>();
+    ASSERT_NE(&el1, &el2); // sanity check
+    ASSERT_NE(el1.data(), el2.data());
+    ASSERT_EQ(el1.front(), el2.front());
+}
 
 /********************************************************************
  * Test class when no template arguments are provided (ply file     *
@@ -163,124 +189,6 @@ TEST_F(FastPlyEmptyElements, PointerState)
 // {
 //     auto path = std::string("test_no_element.ply");
 //     ASSERT_EQ(fp->open(path), false);
-// }
-
-
-// TEST_F(FastPlyBasicFunctionality, DataReadOutLittleEndian)
-// {
-//     auto path = std::string("test_many.ply");
-//     ASSERT_EQ(fp->open(path), true);
-
-//     int count = 0;
-
-//     // Validate regular element with content
-//     ASSERT_EQ(fp->get<Vertex>().size, 1232);
-//     for(std::size_t i = 0; i < fp->get<Vertex>().size(); ++i) {
-//         ASSERT_EQ(fp->get<Vertex>()[i].x, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].y, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].z, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].nx, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].ny, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].nz, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].red, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].green, count++%128);
-//         ASSERT_EQ(fp->get<Vertex>()[i].blue, count++%128);
-//     }
-
-//     // Validate single entry element
-//     ASSERT_EQ(fp->get<Camera>().size(), 1);
-//     for(std::size_t i = 0; i < fp->get<Camera>().size; ++i) {
-//         ASSERT_EQ(fp->get<Camera>()[i].view_px, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].view_py, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].view_pz, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].x_axisx, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].x_axisy, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].x_axisz, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].y_axisx, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].y_axisy, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].y_axisz, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].z_axisx, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].z_axisy, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].z_axisz, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].focal, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].scalex, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].scaley, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].centerx, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].centery, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].viewportx, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].viewporty, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].k1, count++%128);
-//         ASSERT_EQ(fp->get<Camera>()[i].k2, count++%128);
-//     }
-
-//     // Validate empty element
-//     ASSERT_EQ(fp->get<Alltypes>().size(), 0);
-
-//     // Validate list
-//     ASSERT_EQ(fp->get<Face>().size(), 15);
-//     for(std::size_t i = 0; i < fp->get<Face>().size(); ++i) {
-//         ASSERT_EQ(static_cast<unsigned int>(fp->get<Face>()[i].list_length), 4);
-//         for(std::size_t j = 0; j < static_cast<unsigned int>(fp->get<Face>()[i].list_length); ++j)
-//         {
-//             ASSERT_EQ(fp->get<Face>()[i].vertex_index[j], count++%128);
-//         }
-//     }
-// }
-
-
-// TEST_F(FastPlyBasicFunctionality, FilledElementAtRangeCheck)
-// {
-//     // Ply with at least one element and 2 or more entries for that element
-//     auto path = std::string("test15117.ply");
-//     ASSERT_EQ(fp->open(path), true);
-//     ASSERT_THROW(fp->get<Vertex>().at(-1), std::out_of_range);
-//     ASSERT_THROW(fp->get<Vertex>().at(15117), std::out_of_range);
-//     ASSERT_THROW(fp->get<Vertex>().at(fp->get<Vertex>().size()), std::out_of_range);
-//     ASSERT_NO_THROW(fp->get<Vertex>().at(0));
-//     ASSERT_NO_THROW(fp->get<Vertex>().at(15116));
-//     ASSERT_NO_THROW(fp->get<Vertex>().at(fp->get<Vertex>().size() - 1));
-// }
-
-// TEST_F(FastPlyBasicFunctionality, BigEndian)
-// {
-//     auto path = std::string("test_big_endian.ply");
-
-//     ASSERT_EQ(fp->isBigEndian(), false);
-//     ASSERT_EQ(fp->isHeaderParsed(), false);
-//     ASSERT_EQ(fp->open(path), true);
-//     ASSERT_EQ(fp->isHeaderParsed(), true);
-//     ASSERT_EQ(fp->isBigEndian(), true);
-// }
-
-// TEST_F(FastPlyBasicFunctionality, HeaderOffset)
-// {
-//     auto path = std::string("test_big_endian.ply");
-//     ASSERT_EQ(fp->open(path), true);
-//     ASSERT_EQ(fp->getHeaderOffset(), 229);
-//     path = std::string("test_little_endian.ply");
-//     ASSERT_EQ(fp->open(path), true);
-//     ASSERT_EQ(fp->getHeaderOffset(), 232);
-// }
-
-// TEST_F(FastPlyBasicFunctionality, DataSizes)
-// {
-//     auto path = std::string("test_big_endian.ply");
-//     ASSERT_EQ(fp->open(path), true);
-//     ASSERT_EQ(fp->getHeaderOffset(), 229);
-//     ASSERT_EQ(fp->getVertexHeader().size(), 1337);
-//     ASSERT_EQ(fp->getVertexHeader().size()T, 27);
-// }
-
-// TEST_F(FastPlyBasicFunctionality, DataCheck)
-// {
-//     auto path = std::string("test_little_endian.ply");
-//     ASSERT_EQ(fp->open(path), true);
-//     ASSERT_FLOAT_EQ(fp->getVertexHeader().start->x, 0.15807361694047417);
-// }
-
-// int main(int argc, char **argv) {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
 // }
 
 int main(int argc, char **argv) {
